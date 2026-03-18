cdea39398b2efc5b8533f54681d16327a1bc5f6a
dieguinlaredin <diegolaredo45@gmail.com>
2026-03-17 23:17:15 -0600

    Add files via upload

---
+++ queries/auth_queries.py
@@ -0,0 +1,53 @@
+from database.connection import get_connection
+
+
+def crear_usuario(email: str, passwd: str) -> dict:
+    connection = get_connection()
+    if not connection:
+        return {"ok": False, "error": "No se pudo conectar a la BD"}
+
+    cursor = None
+
+    try:
+        cursor = connection.cursor()
+        cursor.execute(
+            """
+            INSERT INTO usuarios (email, passwd, rol_id)
+            VALUES (%s, %s, %s)
+            """,
+            (email, passwd, 3),
+        )
+        connection.commit()
+
+        return {"ok": True, "usuario_id": cursor.lastrowid}
+
+    except Exception as ex:
+        try:
+            connection.rollback()
+        except Exception:
+            pass
+        print(f"Error al crear usuario: {ex}")
+        return {"ok": False, "error": "Error al crear usuario"}
+
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+def obtener_clientes():
+    connection = get_connection()
+    cursor = connection.cursor(dictionary=True)
+
+    cursor.execute("""
+        SELECT usuario_id, email, rol_id
+        FROM usuarios
+        WHERE rol_id = 3
+    """)
+
+    data = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return data
